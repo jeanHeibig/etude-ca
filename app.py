92a27efb0dbbdf66e71f8781d8709947494b747a
jean-heibig <jean.heibig@eleves.enpc.fr>
2022-08-24 14:20:33 +0200

    Changed df name

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ uploaded_file = st.file_uploader("Choose a file", type="csv", accept_multiple_fi
 
 if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.dataframe(dataframe)
+    user_df = pd.read_csv(uploaded_file)
+    st.dataframe(user_df)
