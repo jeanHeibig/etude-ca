e1d1b92e2a916600cd9a741ccd0977daa9c555cd
life-echecs <contact@life-echecs.org>
2022-08-24 10:44:32 +0200

    Removed '*' char from uploader call

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import streamlit as st
 
 
-uploaded_file = st.file_uploader("Choose a file", type="csv", accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False)
+uploaded_file = st.file_uploader("Choose a file", type="csv", accept_multiple_files=False, key=None, help=None, on_change=None, args=None, kwargs=None, disabled=False)
 
 if uploaded_file is not None:
     # Can be used wherever a "file-like" object is accepted:
