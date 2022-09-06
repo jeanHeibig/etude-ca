86fc54af22072060c17612e0e3e503c0816fd5d6
jean-heibig <jean.heibig@eleves.enpc.fr>
2022-09-06 11:22:46 +0200

    Removed unused Django settings

--- app.py
+++ app.py
@@ -1,13 +1,8 @@
 import os
 
-# from django.core.wsgi import get_wsgi_application
 import streamlit as st
 
 from analysis import main
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-
-# application = get_wsgi_application()
 
 from django.contrib.auth import authenticate
 
