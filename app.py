e22617005dd69442cb7a2f2caa37747fa42b9b0d
jean-heibig <jean.heibig@eleves.enpc.fr>
2022-09-12 17:23:53 +0200

    Added back Django config

--- app.py
+++ app.py
@@ -4,6 +4,11 @@ import streamlit as st
 
 from analysis import main
 
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+
+application = get_wsgi_application()
 from django.contrib.auth import authenticate
 
 
